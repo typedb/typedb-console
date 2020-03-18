07a569a9c6142e7ee6b48d699eba15b332ddf92a
Joshua Send <joshua@grakn.ai>
2020-03-18 14:03:35 +0000

    Add ability to list and delete keyspaces from console (#24)
    ## What is the goal of this PR?
    List, Delete existing keyspaces from console. Previously we had to spin up an interactive python console or something similar to see what keyspaces even exist in a distribution, let alone delete them.
    To access the new commands, we would run in any console session:
    ```
    grakn> keyspace list
    ```
    which should print the list of keyspaces on the grakn distribution the console is connected to.
    
    Similarly, we can do:
    ```
    grakn> keyspace delete somekeyspace
    Successfully deleted somekeyspace
    ```
    
    ## What are the changes implemented in this PR?
    * Add `keyspace list` and `keyspace delete [keyspace]` as a commands in console
    * Add test for `keyspace list` and `delete`

--- ConsoleSession.java
+++ ConsoleSession.java
@@ -25,8 +25,8 @@ import graql.lang.Graql;
 import graql.lang.query.GraqlQuery;
 import io.grpc.StatusRuntimeException;
 import jline.console.ConsoleReader;
-import jline.console.history.History;
 import jline.console.history.FileHistory;
+import jline.console.history.History;
 import jline.console.history.MemoryHistory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +42,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.apache.commons.lang.StringEscapeUtils.unescapeJava;
@@ -64,6 +65,11 @@ public class ConsoleSession implements AutoCloseable {
     private static final String CLEAR = "clear";
     private static final String EXIT = "exit";
     private static final String CLEAN = "clean";
+    private static final String KEYSPACE = "keyspace";
+
+    // keyspace sub-commands
+    private static String LIST = "list";
+    private static String DELETE = "delete";
 
     private static final String ANSI_PURPLE = "\u001B[35m";
     private static final String ANSI_RESET = "\u001B[0m";
@@ -170,7 +176,12 @@ public class ConsoleSession implements AutoCloseable {
             } else if (input.equals(EXIT)) {
                 consoleReader.flush();
                 return;
-
+            } else if (input.startsWith(KEYSPACE)) {
+                boolean exit = keyspaceCommand(input.substring(KEYSPACE.length()));
+                if (exit) {
+                    consoleReader.flush();
+                    return;
+                }
             } else if (!input.isEmpty()) {
                 executeQuery(input);
 
@@ -320,7 +331,6 @@ public class ConsoleSession implements AutoCloseable {
             builder = new ProcessBuilder("/bin/bash", "-c", editor + " </dev/tty >/dev/tty " + tempFile.getAbsolutePath());
         }
 
-
         builder.start().waitFor();
         return String.join("\n", Files.readAllLines(tempFile.toPath()));
     }
@@ -330,4 +340,31 @@ public class ConsoleSession implements AutoCloseable {
         return System.getProperty("os.name").toLowerCase().contains("win");
     }
 
+    /**
+     * @return boolean indicating whether the current keyspace has been deleted and we need to exit
+     */
+    private boolean keyspaceCommand(String subCommand) throws IOException {
+        String command = subCommand.trim();
+        if (command.equals(LIST)) {
+            List<String> keyspaces = client.keyspaces().retrieve().stream().sorted().collect(Collectors.toList());
+            for (String ksp : keyspaces) {
+                consoleReader.println(ksp);
+            }
+        } else if (command.startsWith(DELETE + ' ')) {
+            String keyspaceToDelete = command.substring(DELETE.length() + 1).trim();
+            List<String> keyspaces = client.keyspaces().retrieve();
+            if (!keyspaces.contains(keyspaceToDelete)) {
+                consoleReader.println("Keyspace " + keyspaceToDelete + " does not exist");
+                return false;
+            }
+            if (keyspaceToDelete.equals(session.keyspace().toString())) {
+                // redirect to clean() with confirmation, return status of clean()
+                // if we cleaned, we need to exit
+                return clean();
+            }
+            client.keyspaces().delete(keyspaceToDelete);
+            consoleReader.println("Successfully deleted keyspace: " + keyspaceToDelete);
+        }
+        return false;
+    }
 }

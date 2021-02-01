bf2d049db96d0201816803ef2a4653199ce56669
vladgan <vladyslav@grakn.ai>
2021-02-01 18:40:49 +0000

    Added message

--- GraknConsole.java
+++ GraknConsole.java
@@ -269,7 +269,8 @@ public class GraknConsole {
         } catch (CancellationException e) {
             Instant end = Instant.now();
             printer.info("answers: " + counter + ", duration: " + Duration.between(start, end).toMillis() +" ms");
-            executorService.shutdown();
+
+            printer.info("Stream was shut down in the console, but the Grakn Server might still be working on last answers for some time.");
         } finally {
             terminal.handle(Terminal.Signal.INT, Terminal.SignalHandler.SIG_IGN);
         }

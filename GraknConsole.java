21371dbd12bc370f57f9db0370dc58a98e0ed1e4
vladgan <vladyslav@grakn.ai>
2021-02-02 10:34:24 +0000

    updated message

--- GraknConsole.java
+++ GraknConsole.java
@@ -268,7 +268,7 @@ public class GraknConsole {
             Instant end = Instant.now();
             printer.info("answers: " + counter + ", duration: " + Duration.between(start, end).toMillis() +" ms");
 
-            printer.info("The stream was shut down in the console, but the Grakn Server might still be working on the last answers for some time.");
+            printer.info("The query has been cancelled. It may take some time for the cancellation to finish on the server side.");
         } finally {
             terminal.handle(Terminal.Signal.INT, Terminal.SignalHandler.SIG_IGN);
         }

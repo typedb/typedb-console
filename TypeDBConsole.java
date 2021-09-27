8603c628955034cb34eb402ce19c30c75d937fa0
joshua <joshuasend1@gmail.com>
2021-09-27 16:13:21 +0100

    rename methods to be symmetric

--- TypeDBConsole.java
+++ TypeDBConsole.java
@@ -310,7 +310,7 @@ public class TypeDBConsole {
                     } else if (replCommand.isSource()) {
                         runSource(tx, replCommand.asSource().file(), replCommand.asSource().printAnswers());
                     } else if (replCommand.isQuery()) {
-                        runQueriesPrintAnswers(tx, replCommand.asQuery().query());
+                        runQueriesPrint(tx, replCommand.asQuery().query());
                     }
                 }
             }
@@ -397,7 +397,7 @@ public class TypeDBConsole {
                                     boolean success = runSource(tx, source.file(), source.printAnswers());
                                     if (!success) return false;
                                 } else if (txCommand.first().isQuery()) {
-                                    boolean success = runQueriesPrintAnswers(tx, txCommand.first().asQuery().query());
+                                    boolean success = runQueriesPrint(tx, txCommand.first().asQuery().query());
                                     if (!success) return false;
                                 } else {
                                     printer.error("Command is not available while running console script.");
@@ -586,30 +586,30 @@ public class TypeDBConsole {
     private boolean runSource(TypeDBTransaction tx, String file, boolean printAnswers) {
         try {
             String queryString = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
-            if (printAnswers) return runQueriesPrintAnswers(tx, queryString);
-            else return runQueriesAsync(tx, queryString);
+            if (printAnswers) return runQueriesPrint(tx, queryString);
+            else return runQueriesNoPrint(tx, queryString);
         } catch (IOException e) {
             printer.error("Failed to open file '" + file + "'");
             return false;
         }
     }
 
-    private boolean runQueriesPrintAnswers(TypeDBTransaction tx, String queryString) {
+    private boolean runQueriesPrint(TypeDBTransaction tx, String queryString) {
         Optional<List<TypeQLQuery>> queries = parseQueries(queryString);
         if (!queries.isPresent()) return false;
-        queries.get().forEach(query -> runQueryPrintAnswers(tx, query));
+        queries.get().forEach(query -> runQueryPrint(tx, query));
         return true;
     }
 
-    private boolean runQueriesAsync(TypeDBTransaction tx, String queryString) {
+    private boolean runQueriesNoPrint(TypeDBTransaction tx, String queryString) {
         Optional<List<TypeQLQuery>> queries = parseQueries(queryString);
         if (!queries.isPresent()) return false;
-        CompletableFuture.allOf(queries.get().stream().map(query -> runQueryAsync(tx, query))
+        CompletableFuture.allOf(queries.get().stream().map(query -> runQueryNoPrint(tx, query))
                 .toArray(CompletableFuture[]::new)).join();
         return true;
     }
 
-    private void runQueryPrintAnswers(TypeDBTransaction tx, TypeQLQuery query) {
+    private void runQueryPrint(TypeDBTransaction tx, TypeQLQuery query) {
         if (query instanceof TypeQLDefine) {
             tx.query().define(query.asDefine()).get();
             printer.info("Concepts have been defined");
@@ -645,7 +645,7 @@ public class TypeDBConsole {
     }
 
     @SuppressWarnings("CheckReturnValue")
-    private CompletableFuture<Void> runQueryAsync(TypeDBTransaction tx, TypeQLQuery query) {
+    private CompletableFuture<Void> runQueryNoPrint(TypeDBTransaction tx, TypeQLQuery query) {
         if (query instanceof TypeQLDefine) {
             tx.query().define(query.asDefine()).get();
             printer.info("Concepts have been defined");
